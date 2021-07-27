613edabe9d28f0b70b239251214d94b25188b2e6
EddyWaid <designed.waid@gmail.com>
2021-07-27 22:49:13 +0200

    dai che ci siamo quasi

--- cambia.php
+++ cambia.php
@@ -1,6 +1,15 @@
 <?php
-  if (isset(shell_exec('cd /Applications/XAMPP/xamppfiles/htdocs/TEst/Test/ && git pull')))
+  $c = shell_exec('cd /Applications/XAMPP/xamppfiles/htdocs/TEst/Test/ && git pull');
+  echo $c;
+  if (isset($c))
   {
     header('Location: t1.html');
+  }
+  else
+  {
+    echo shell_exec('ls');
+    echo shell_exec('git pull');
+    $c = shell_exec('git pull');
+    echo $c;
   }
 ?>
