93a7288b9d10fa77449a6be697ddf378c7724e4f
EddyWaid <designed.waid@gmai.com>
2021-07-28 01:51:50 +0200

    Finito

--- cambia.php
+++ cambia.php
@@ -2,12 +2,11 @@
   $c = shell_exec('sudo cd /Applications/XAMPP/xamppfiles/htdocs/TEst/Test/ && sudo git reset--hard && sudo git pull 2>&1');
   if (isset($c))
   {
-
+    header('Location: t1.html');
   }
   else
   {
-    echo shell_exec('id -un');
-
-    echo shell_exec('sudo git stash && sudo -S git pull 2>&1');
+    shell_exec(' sudo -S git pull 2>&1');
+    header('Location: t1.html');
   }
 ?>
