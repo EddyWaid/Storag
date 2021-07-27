2ea4c8e760f2835074a3b06a1422a8cde6b9a645
EddyWaid <designed.waid@gmail.com>
2021-07-27 22:32:25 +0200

    Questa volta funziona

--- cambia.php
+++ cambia.php
@@ -1,5 +1,5 @@
 <?php
-  if (isset(shell_exec('cd Desktop/TEst/Test && git pull'))))
+  if (isset(shell_exec('cd /Applications/XAMPP/xamppfiles/htdocs/TEst/Test/ && git pull'))))
   {
     header('Location: t1.html');
   }
